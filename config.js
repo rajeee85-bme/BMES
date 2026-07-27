2e49a803229818a119436599074120a95bd1649e
rajeee85-bme <rajeee85@gmail.com>
2026-07-27 20:11:09 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// Paste your deployed Apps Script Web App URL below, between the quotes.
+// You get this URL after: Extensions > Apps Script > Deploy > New deployment > Web app
+// It looks like: https://script.google.com/macros/s/AKfycb.../exec
+const API_URL = "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE";

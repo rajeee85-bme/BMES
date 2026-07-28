34df56a4d2db0fae316ee525e39114168d2e08e0
rajeee85-bme <rajeee85@gmail.com>
2026-07-28 15:15:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbwxVojvth9uNwOb07zSqJEDVRqmwV3fQLrZTYuLFhRtedFlzgDRcangHIQeeUA6lCjF/exec"
+const API_URL = "https://script.google.com/macros/s/AKfycbznhUShIiOqMryOo2I1VI1QRB4iqCe8xmfTimcmTQDsGyAx3dHsyDG485yU0VL9b_3d/exec"
